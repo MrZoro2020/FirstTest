6ea5c580b3ecf3abb8cea5169c001a7039b203b2
Zoro <Zoro@DESKTOP-9O2D1TB>
2023-10-04 16:57:36 +1100

    new codes added -> daughter's age

--- MyTest/src/test/java/tests/Main.java
+++ MyTest/src/test/java/tests/Main.java
@@ -4,7 +4,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
+		int age = 6;
+		System.out.println("hello world!!! ");
 		
+		System.out.println("How old is my daughter?");
+		System.out.println("your daughter is "+ age);
 		
 	}
 
